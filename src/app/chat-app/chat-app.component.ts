eeec0c9738d104fc584bddf52e66b20445a94a51
alaaelhariri <ashariri95@gmail.com>
2017-11-17 22:16:26 +0200

    feat (chat-app) : add deepstream service

--- src/app/chat-app/chat-app.component.ts
+++ src/app/chat-app/chat-app.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Form} from '@angular/forms';
 
+import {DeepStreamService} from '../services/deep-stream.service'
+
 @Component({
   selector: 'ca-hg-chat-app',
   templateUrl: './chat-app.component.html',
   styleUrls: ['./chat-app.component.css']
 })
 export class ChatAppComponent implements OnInit {
+
+  username = 'a_chatter';
 
   chat_array = [
     {
@@ -22,24 +26,29 @@ export class ChatAppComponent implements OnInit {
 
   sendChatForm: FormGroup;
 
-  constructor(private _fb: FormBuilder ) { }
+  constructor(private _fb: FormBuilder, private _dss: DeepStreamService ) { }
 
   ngOnInit() {
+    // initialize message form
     this.sendChatForm = this._fb.group(
       {
-        username: [],
         message: []
       }
-    )
+    );
+
+    //get username from user
+    this.username = window.prompt('Please enter a username', this.username);
+
+    this._dss.login(null, this.handleLogin);
+
+
   }
 
-  addChat(usr, msg) {
-    this.chat_array.push(
-      {
-        username : usr,
-        message : msg
-      }
-    );
+  handleLogin(success, data) {
+    console.log('logged in', success, data)
+  }
+
+  addChat(msg) {
   }
 
 }
