fd0234c2bfec4deb03da41de91226c32b200e28e
alaaelhariri <ashariri95@gmail.com>
2017-11-17 22:15:32 +0200

    feat (deep-stream): create and add login to deepstream services

--- src/app/services/deep-stream.service.ts
+++ src/app/services/deep-stream.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+declare var deepstream: any;
 
 @Injectable()
 export class DeepStreamService {
 
-  constructor() { }
+  private deepS;
+  public deepSInstance;
 
+  constructor() {
+    this.deepS = this.deepSInstance = deepstream('localhost:6020');
+  }
+
+  login (credentials?, loginHandler?) {
+    //allows you to "log in" to the server created by deepstream
+    this.deepS.login(credentials, loginHandler)
+  }
 }
+
